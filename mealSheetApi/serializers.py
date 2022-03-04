b793cbcbfc4e346d54642074229746a64393d828
Steffen <kaemmi84@gmx.de>
2022-03-04 04:37:52 +0100

    change serializer

--- mealSheetApi/serializers.py
+++ mealSheetApi/serializers.py
@@ -1,9 +1,8 @@
-from dataclasses import fields
 from rest_framework import serializers
-from  .models import Meal
+from .models import Meal
+
 
 class MealSerializer(serializers.ModelSerializer):
-
     class Meta:
-        model =  Meal
-        fields = ("id", "description", "owner", "updated")
+        model = Meal
+        fields = ("id", "description", "owner", "updated", "picture")
