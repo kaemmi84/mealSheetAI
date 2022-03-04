96d506623d54860a1c3527f6dfa4ae6c58200baa
Steffen <kaemmi84@gmx.de>
2022-03-04 10:00:34 +0100

    added cors

--- mealSheetAI/settings.py
+++ mealSheetAI/settings.py
@@ -39,18 +39,25 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'mealSheetApi',
     'rest_framework',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8080',
+)
 
 ROOT_URLCONF = 'mealSheetAI.urls'
 
